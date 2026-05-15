c2d3739abc4da5002d4d0ff885cb427d38b8c847
lilahmd <miramenomemires16@gmail.com>
2026-05-15 13:05:25 +0200

    fix cors para vercel

--- src/index.js
+++ src/index.js
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: ['https://azuky-gym.vercel.app', 'http://localhost:5173'],
+  credentials: true
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 
